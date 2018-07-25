7b4ab72b382c7f2eea6a59a5a1e9213bdb0ab34b
nimetbetul <nbkaratas@students.chccs.k12.nc.us>
2018-07-25 13:06:20 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,17 +9,17 @@ STUDENT_NAMES = [
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
-puts STUDENT_NAMES.[0] 
+puts STUDENT_NAMES.['0'] 
 end
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
-  puts STUDENT_NAMES.[3]
+  puts STUDENT_NAMES.['3']
 end
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
-  puts STUDENT_NAMES.[5]
+  puts STUDENT_NAMES.['5']
 end
 
 def first_student_by_method
@@ -34,5 +34,5 @@ end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-  puts STUDENT_NAMES.[0,1,2]
+  puts STUDENT_NAMES.['0','1','2']
 end
